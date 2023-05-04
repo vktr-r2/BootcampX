cd9bdaf99b1b4c221544c0025fb89f2fbc24ccea
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-03 23:45:24 -0400

    Add teachers_assisted_in_cohort.sql query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,3 +6,17 @@ const pool = new Pool({
   host: 'localhost',
   database:'bootcampx'
 });
+
+const cohort = process.argv[2]
+
+pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name LIKE '%${cohort}%'
+    ORDER BY teacher;`)
+.then(res => {
+  console.log(res.rows);
+})
+.catch(err => console.error('query error', err.stack));
