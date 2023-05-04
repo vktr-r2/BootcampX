090b09d0b48b02c96b200741f994394a2e0cce01
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-04 11:33:58 -0400

    Update teachers.js to use parameterized queries to guard against injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,14 +8,18 @@ const pool = new Pool({
 });
 
 const cohort = process.argv[2]
-
-pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const values = [`%${cohort}%`]
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-  WHERE cohorts.name LIKE '%${cohort}%'
-    ORDER BY teacher;`)
+  WHERE cohorts.name LIKE $1
+    ORDER BY teacher;
+    `
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
 })
